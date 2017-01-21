fc37ba337dd39f4147789ddb97e7d86b31026509
chadjougit <deftones@protonmail.com>
2017-01-21 21:53:09 +0300

    newcommit
    my first commit in pw

---
+++ src/PWappServer/Hubs/StaticList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWappServer.Hubs
+{
+    public class MyUser
+    {
+        public string ConnectionId;
+        public string Username;
+
+    }
+
+
+    public static class StaticList
+    {
+
+        public static List<MyUser> MyList = new List<MyUser>();
+        /*
+        static void Add(string text)
+        { MyList.Add(text); }
+        */
+
+    }
+}
